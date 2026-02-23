3d0d525d1a9e601b47916562b0e7050c017921b3
npc <creative.minarai@gmail.com>
2026-02-23 22:43:42 +0900

    喋って瞬きしてる

--- project/arles/chat-data.js
+++ project/arles/chat-data.js
@@ -63,6 +63,7 @@ const chatScenarios = {
     },
 
     "bread_top": {
+        speaker: "tenshu_bread",
         text: [
             "[[８０年酵母]]の[[ルヴァン種]]を使用した自慢のパンたちです。",
             "どのパンに興味がありますか？"
@@ -76,6 +77,7 @@ const chatScenarios = {
     },
 
     "croissant_detail": {
+        speaker: "tenshu_bread",
         text: [
             "生地から手作りしている自慢の[[クロワッサン]]です！[[img:sample_croissant.jpg]]",
             "バターは[[明治発酵バター]]を使用しています。（※昨今のバター品薄状態により、日高バターを使用する場合もあります）"
@@ -88,6 +90,7 @@ const chatScenarios = {
     },
 
     "croissant_taste": {
+        speaker: "tenshu_bread",
         text: [
             "絶妙な[[18層]]に仕上げています！",
             "外は[[サクサク]]、中は[[もっちり]]。",
@@ -101,6 +104,7 @@ const chatScenarios = {
     },
 
     "croissant_reheat": {
+        speaker: "tenshu_bread",
         text: [
             "当日の[[冷めた状態]]が最も食べ頃です！サクサク感と香ばしい香りがたまりません。",
             "翌日以降は[[アルミホイル]]を被せてトースターで焼いてみてください！",
@@ -114,6 +118,7 @@ const chatScenarios = {
     },
 
     "hard_bread": {
+        speaker: "tenshu_bread",
         text: [
             "私たちの誇り、[[ルヴァン種のカンパーニュ]]です！[[img:levain_campange.JPG]]",
             "100%ルヴァン種で作っているので、その[[酸味と旨味]]を存分に味わうならぜひこちらをどうぞ。",
@@ -130,6 +135,7 @@ const chatScenarios = {
     },
 
     "hard_bread_taste": {
+        speaker: "tenshu_bread",
         text: [
             "「酸っぱいパンは苦手！」と思っていたお客様も、このカンパーニュはぜひ一度お試しください。",
             "パンにおける[[酸味の概念]]が変わりますよ。",
@@ -144,6 +150,7 @@ const chatScenarios = {
     },
 
     "hard_bread_process": {
+        speaker: "tenshu_bread",
         text: [
             "焼き上がるまでに[[丸3日間]]の熟成が必要で、仕込んでから[[4日目]]にようやく完成する手間暇かかったパンなんです。",
             "毎日種を[[『リフレッシュ』]]して新鮮な状態で使う、[[８０年守り抜いた酵母]]の魔法が詰まっています！"
@@ -156,6 +163,7 @@ const chatScenarios = {
     },
 
     "daily_bread": {
+        speaker: "tenshu_bread",
         text: [
             "もっちり食感の[[『湯種食パン』]]や、バターをたっぷり包み込んだ[[『塩パン』]]など、毎日の食卓をちょっと贅沢にしてくれる、私たちの自信作です！",
             "どちらのお話を聞きたいですか？"
@@ -168,6 +176,7 @@ const chatScenarios = {
     },
 
     "sio_pan": {
+        speaker: "tenshu_bread",
         text: [
             "こちらの[[塩パン]]は、バターをたっぷり贅沢に包み込んで焼き上げています！[[img:siopan.jpg]]",
             "生地には[[湯種]]を使用しているので、外はカリッと、中は驚くほどもっちりとした食感に仕上がっていますよ。"
@@ -179,6 +188,7 @@ const chatScenarios = {
     },
 
     "sweet_bread": {
+        speaker: "tenshu_bread",
         text: [
             "おやつにぴったりの菓子パンもご用意しています！特におすすめなのがこちらの[[特製メロンパン]]です。[[img:melon_pan.JPG]]",
             "外のクッキー生地はサクサク、中はふんわりもっちり。優しい甘さが口いっぱいに広がりますよ！"
@@ -190,6 +200,7 @@ const chatScenarios = {
     },
 
     "shelf_life": {
+        speaker: "tenshu_bread",
         text: [
             "実はルヴァン100%のカンパーニュ、驚くほど長持ちするんです！",
             "[[冬なら1ヶ月]]、[[夏でも2〜3週間]]は美味しく食べられます。",
